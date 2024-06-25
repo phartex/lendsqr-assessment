9f743c8f566f02ae83ab6b0fdc51b4e79b2d3d54
fateru tobi <fateruvictor96@gmail.com>
2024-06-25 10:50:13 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import User from './pages/User';
 import UserDetails from './pages/UserDetails';
 import SideMenu from './components/SideMenu';
 import Login from './pages/Login';
-import './App.css';
+import './App.css'; 
 import Navbar from './pages/NavBar';
 import DashBoard from './pages/DashBoard';
 
@@ -26,18 +25,18 @@ const App: React.FC = () => {
       <div className="app-container">
         {isAuthenticated ? (
           <>
-            <Navbar onToggleMenu={handleToggleMenu}/>
-            <SideMenu isOpen={isMenuOpen}/>
+            <Navbar onToggleMenu={handleToggleMenu} />
+            <SideMenu isOpen={isMenuOpen} />
             <div className="main-content">
               <Routes>
                 <Route path="/dashboard" element={<DashBoard />} />
                 <Route path="/users" element={<User />} />
                 <Route path="/users/:id" element={<UserDetails />} />
-                <Route path="/" element={<DashBoard />} />
               </Routes>
             </div>
           </>
         ) : (
+          
           <Routes>
             <Route path="/" element={<Login onLogin={handleLogin} />} />
             <Route path="/login" element={<Login onLogin={handleLogin} />} />
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -13,14 +13,21 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    console.log('---')
     onLogin();
     navigate('/dashboard');
   };
 
   return (
     <div className="login-container">
+     
+      <div className="login-image">
+        <img src="/assets/login-image.svg" alt="Enterprise Resource Planning System" />
+      </div>
+
       <div className="login-form-container">
-        <h2 className='heading-color'>Login to your accounts</h2>
+        <h2 className='heading-color'>Welcome !</h2>
+        <p className='heading-color'>Enter details to login</p>
         <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Email</label>
@@ -44,12 +51,10 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
               required
             />
           </div>
+          <a href="/forgot-password">FORGOT PASSWORD?</a>
           <button type="submit">Log In</button>
         </form>
-        <a href="/forgot-password">Forgot Password?</a>
-      </div>
-      <div className="login-image">
-        <img src="/path-to-your-image.png" alt="Enterprise Resource Planning System" />
+        
       </div>
     </div>
   );
